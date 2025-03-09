4797d6b7ad67b52343c118592ce426be5352fe86
Spectrasonic <spectrasonic117@gmail.com>
2025-03-09 12:42:30 -0600

    feat : add item builder and command to get stick

--- src/main/java/com/spectrasonic/QLTML/QLTMLCommand.java
+++ src/main/java/com/spectrasonic/QLTML/QLTMLCommand.java
@@ -8,6 +8,8 @@ import co.aikar.commands.annotation.Syntax;
 import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import com.spectrasonic.QLTML.Utils.ItemBuilder;
 
 @RequiredArgsConstructor
 @CommandAlias("qltml")
@@ -27,5 +29,26 @@ public class QLTMLCommand extends BaseCommand {
         } else {
             sender.sendMessage(MiniMessage.miniMessage().deserialize("<yellow>Uso: /qltml game <on|off>"));
         }
+    }
+
+    @Subcommand("give")
+    @CommandCompletion("stick")
+    public void onGiveStick(CommandSender sender) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(MiniMessage.miniMessage().deserialize("<red>Este comando solo puede ser usado por jugadores"));
+            return;
+        }
+        
+        Player player = (Player) sender;
+        
+        ItemBuilder flyingStick = ItemBuilder.setMaterial("stick")
+                .setName("<gold><bold>Palo Volador")
+                .setLore("<gray>Un palo mágico que te hace volar", 
+                         "<gray>¡Golpea a tus enemigos para verlos salir volando!")
+                .addEnchantment("knockback", 5)
+                .setCustomModelData(1);
+                
+        player.getInventory().addItem(flyingStick.build());
+        player.sendMessage(MiniMessage.miniMessage().deserialize("<green>¡Has recibido un <red>Palo Volador</red>!"));
     }
 }
